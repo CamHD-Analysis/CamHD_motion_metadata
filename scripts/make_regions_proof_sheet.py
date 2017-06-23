47071807703e25cd400eb00ac662a49c58395c5d
Aaron Marburg <amarburg@apl.washington.edu>
2017-06-23 11:22:34 -0700

    Refactoring make_regions_proof_sheet to use multiple files rather than a fixed ground truth.

--- scripts/make_regions_proof_sheet.py
+++ scripts/make_regions_proof_sheet.py
@@ -16,15 +16,15 @@ import region_analysis as ra
 
 parser = argparse.ArgumentParser(description='Generate HTML proofs')
 
-parser.add_argument('input', metavar='N', nargs='*',
-                    help='Files or paths to process')
+parser.add_argument('input', metavar='inputfiles', nargs='+',
+                    help='Regions files to process')
 
 parser.add_argument('--force', dest='force', action='store_true', help='Force re-download of images')
 
 parser.add_argument('--log', metavar='log', nargs='?', default='INFO',
                     help='Logging level')
 
-parser.add_argument('--output-dir', dest='outdir', nargs='?', default='_html/', help='Directory for output')
+parser.add_argument('--output', dest='outfile', nargs='?', default='_html/proof.html', help='Output .html file')
 
 parser.add_argument('--lazycache-url', dest='lazycache', default=os.environ.get("LAZYCACHE_URL", "http://camhd-app-dev-nocache.appspot.com/v1/org/oceanobservatories/rawdata/files"),
                     help='URL to Lazycache repo server (only needed if classifying)')
@@ -40,69 +40,132 @@ qt = camhd.lazycache( args.lazycache )
 # classifier = ra.Classifier()
 # classifier.load( "classification/images/" )
 
+tags = []
+images = []
+urls = []
 
 for pathin in args.input:
     for infile in glob.iglob( pathin, recursive=True):
 
-
         with open(infile) as data_file:
             jin = json.load( data_file )
 
-        mov = jin["movie"]
-        url = mov['URL']
-        name = path.splitext( path.basename( url ))[0]
+            if 'regions' not in jin:
+                logging.info("No regions in file %s" % infile)
+                continue
 
-        html_file = args.outdir + "%s.html" % name
-        img_path  = args.outdir + name + "/"
+            url = jin['movie']['URL']
+            urls.append(url)
 
-        os.makedirs( img_path, exist_ok = True )
-
-
-        with open(html_file, 'w') as html:
-            html.write("<html><body>\n")
-            html.write("<h2>%s</h2>\n\n" % name)
-
-            html.write("<table>\n<tr><th>Start Frame</th><th>End Frame</th><th>Classification</th><th>Sample Image</th</tr>\n")
-            #><th>Reference image from class</th>\n")
-
+            idx = 0
             for r in jin["regions"]:
-
-                if r['type'] != "static":
+                if r['type'] != 'static':
                     continue
 
-                html.write("<tr>")
-                html.write("<td>%d</td><td>%d</td>" % (r['startFrame'], r['endFrame']) )
+                if 'sceneTag' not in r:
+                    logging.info("Hm, static regions aren't tagged")
+                    continue
 
-                if "sceneTag" in r.keys():
-                    html.write("<td>%s</td>" % r["sceneTag"])
+                sceneTag = r['sceneTag']
+
+                if idx >= len(images):
+                    tags.append(sceneTag)
+                    images.append( { url: r })
+                    idx += 1
+                elif sceneTag == tags[idx]:
+                    images[idx][url] = r
                 else:
-                    html.write("<td><<b>unclassified</b></td>")
-
-                sample_frame = r['startFrame'] + 0.5 * (r['endFrame'] - r['startFrame'])
-
-                img_file = img_path + "%d.png" % sample_frame
-                if args.force or not path.exists( img_file ):
-                    logging.info("Fetching frame %d from %s for contact sheet" % (sample_frame, name))
-                    img = qt.get_frame( url, sample_frame, format='png' )
-                    img.save( img_file )
-
-                relapath = path.relpath( img_file, path.dirname(html_file) )
-                html.write("<td><img width=640 src=\"%s\"/></td>" % (relapath) )
-
-                # html.write("<td>")
-                # if "sceneTag" in r.keys() and len(r['sceneTag']) > 0:
-                #     first = r['sceneTag'][0]
-                #     if  first in classifier.tags():
-                #         paths = classifier.sample_paths( first, 3 )
-                #
-                #         relapaths = [ path.relpath(p, path.dirname(html_file) ) for p in paths]
-                #
-                #         for r in relapaths:
-                #             html.write("<img width=640 src=\"%s\"/>" % r)
-                #
-                # html.write("</td>")
-                html.write("</tr>\n")
+                    logging.info("Tag doesn't match order...")
 
 
-            html.write("</table>")
-            html.write("</body></html>")
+
+
+
+        #
+        # mov = jin["movie"]
+        # url = mov['URL']
+        # name = path.splitext( path.basename( url ))[0]
+        #
+        # html_file = args.outdir + "%s.html" % name
+
+img_path  = path.dirname( args.outfile ) + "/images/"
+os.makedirs(img_path, exist_ok=True)
+
+
+html_file = args.outfile
+with open(html_file, 'w') as html:
+    html.write("<html><body>\n")
+    html.write("<h2>Proof sheet</h2>\n\n")
+
+    html.write("<table>\n<tr><th>Tag</th>")
+    for name in urls:
+        html.write("<th>%s</th>" % path.basename(name))
+    html.write("</tr>\n")
+    #><th>Reference image from class</th>\n")
+
+    for row in range(len(images)):
+        html.write("<tr>")
+        html.write("<td>%s</td>" % (tags[row]) )
+
+        for url in urls:
+            if url not in images[row]:
+                html.write("<td></td>")
+                continue
+
+            region = images[row][url]
+
+            sample_frame = region['startFrame'] + 0.5 * (region['endFrame'] - region['startFrame'])
+
+            img_file = img_path + "%s_%d.png" % (path.splitext(path.basename(url))[0], sample_frame)
+            if args.force or not path.exists( img_file ):
+                logging.info("Fetching frame %d from %s for contact sheet" % (sample_frame, name))
+                img = qt.get_frame( url, sample_frame, format='png' )
+                img.save( img_file )
+
+            relapath = path.relpath( img_file, path.dirname(html_file) )
+
+            html.write("<td><img width=640 src=\"%s\"/><br>%d -- %d</td>" % (relapath,region['startFrame'],region['endFrame']) )
+
+
+        html.write("</tr>")
+
+    # for r in jin["regions"]:
+    #
+    #     if r['type'] != "static":
+    #         continue
+    #
+    #     html.write("<tr>")
+    #     html.write("<td>%d</td><td>%d</td>" % (r['startFrame'], r['endFrame']) )
+    #
+    #     if "sceneTag" in r.keys():
+    #         html.write("<td>%s</td>" % r["sceneTag"])
+    #     else:
+    #         html.write("<td><<b>unclassified</b></td>")
+    #
+    #
+    #     img_file = img_path + "%d.png" % sample_frame
+    #     if args.force or not path.exists( img_file ):
+    #         logging.info("Fetching frame %d from %s for contact sheet" % (sample_frame, name))
+    #         img = qt.get_frame( url, sample_frame, format='png' )
+    #         img.save( img_file )
+    #
+    #     relapath = path.relpath( img_file, path.dirname(html_file) )
+    #     html.write("<td><img width=640 src=\"%s\"/></td>" % (relapath) )
+    #
+    #     # html.write("<td>")
+    #     # if "sceneTag" in r.keys() and len(r['sceneTag']) > 0:
+    #     #     first = r['sceneTag'][0]
+    #     #     if  first in classifier.tags():
+    #     #         paths = classifier.sample_paths( first, 3 )
+    #     #
+    #     #         relapaths = [ path.relpath(p, path.dirname(html_file) ) for p in paths]
+    #     #
+    #     #         for r in relapaths:
+    #     #             html.write("<img width=640 src=\"%s\"/>" % r)
+    #     #
+    #     # html.write("</td>")
+    #     html.write("</tr>\n")
+
+
+    html.write("</table>")
+    html.write("</body></html>")
