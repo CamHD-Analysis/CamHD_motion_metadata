6197201088dd7f0e0fa87d3d15910a90b4775696
Aaron Marburg <amarburg@apl.washington.edu>
2017-07-26 10:53:18 -0700

    Added date/time field to CSV file and started Makefile for keeping track of common tasks.

--- datapackage/scripts/validate.py
+++ datapackage/scripts/validate.py
@@ -1,6 +1,11 @@
 from pprint import pprint
 from goodtables import validate
 
-report = validate('../datapackage.json')
+datapackage_file = "datapackage.json"
+
+report = validate(datapackage_file)
 
 pprint(report)
+
+
+exit(0 if report['valid'] else 1)
