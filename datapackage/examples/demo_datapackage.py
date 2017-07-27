6a005127e9713d022a43c9f1b0bdab4a4e1ac5c3
Aaron Marburg <amarburg@apl.washington.edu>
2017-07-27 14:00:37 -0700

    Updated README and demo_datapackage

--- datapackage/examples/demo_datapackage.py
+++ datapackage/examples/demo_datapackage.py
@@ -5,3 +5,10 @@ url = "https://raw.githubusercontent.com/CamHD-Analysis/CamHD_motion_metadata/ma
 dp = datapackage.DataPackage(url)
 
 print(dp.descriptor['title'])
+
+#regions = next(r for r in dp.resources if r.name == 'regions')
+regions = dp.resources[0]
+
+d2_p2_z0 = [r for r in regions.iter() if r['scene_tag'] == 'd2_p2_z0']
+
+print("%d regions" % len(d2_p2_z0))
