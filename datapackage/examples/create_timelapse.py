d2e3478bb4ab03b13072580508c828cc1417e7dd
Friedrich Knuth <knuth@marine.rutgers.edu>
2017-08-15 21:18:06 -0400

    added script to produce time lapse videos

---
+++ datapackage/examples/create_timelapse.py
@@ -0,0 +1,123 @@
+import subprocess
+import os
+import datetime
+
+scene = 'd2_p8_z1'
+
+
+# function to get video duration
+def getLength(filename):
+  result = subprocess.Popen(["ffprobe", filename],
+    stdout = subprocess.PIPE, stderr = subprocess.STDOUT, encoding='utf8')
+  length = [line for line in result.stdout.readlines() if "Duration" in line]
+  length = length[0][12:23]
+  return length
+
+
+# prepare paths
+videos = "output/" + scene + '/videos/'
+frames = "output/" + scene + '/frames/'
+
+
+# create output directory
+if not os.path.exists(videos):
+	os.makedirs(videos)
+
+
+
+# get list of files to parse for time stamps as subtitles
+f = []
+for dirpath, dirnames, filenames in os.walk(frames):
+    f.extend(filenames)
+f = [i[19:35] for i in f]
+
+
+# # create initial timelapse
+# create_vid = "ffmpeg -framerate 10 -pattern_type glob -i '" + frames + \
+# 	"*.png' -c:v libx264 -pix_fmt yuv420p " + videos + scene + "-time_lapse.mp4"
+# process = subprocess.Popen(create_vid,shell=True)
+# process.wait()
+
+
+# # stabilize timelapse
+# detect_vid = "ffmpeg -i " + videos + scene + "-time_lapse.mp4 -vf vidstabdetect -f null -"
+# process = subprocess.Popen(detect_vid,shell=True)
+# process.wait()
+# stab_vid = "ffmpeg -i "+ videos + scene + "-time_lapse.mp4 -vf vidstabtransform " + videos + scene + "-time_lapse_stabilized.mp4"
+# process = subprocess.Popen(stab_vid,shell=True)
+# process.wait()
+
+
+# get input video duration
+filename = videos + scene + "-time_lapse_stabilized.mp4"
+timestring = getLength(filename)
+pt =datetime.datetime.strptime(timestring,'%H:%M:%S.%f')
+total_microseconds = pt.microsecond+pt.second*1e+6+pt.minute*6e+7+pt.hour*3.6e+9
+
+
+# create subtitle srt file
+step = total_microseconds/len(f)
+sub_time = step
+
+srt_file = open(videos + scene + '.srt','w+')
+srt_file.write('1'+'\n'+'0:00:00.00')
+srt_file.write(" --> " + str(datetime.timedelta(microseconds=sub_time))[:-4] +'\n')
+srt_file.write(f[0] + '\n\n')
+
+counter = 2
+for i in f[1:]:
+
+	srt_file.write(str(counter)+'\n')
+
+	if len(str(datetime.timedelta(microseconds=sub_time))) == 14:
+		srt_file.write(str(datetime.timedelta(microseconds=sub_time))[:-4])
+	else:
+		srt_file.write(str(datetime.timedelta(microseconds=sub_time)) + '.00')
+
+	sub_time = sub_time + step
+
+	if len(str(datetime.timedelta(microseconds=sub_time))) == 14:
+		srt_file.write(" --> " + str(datetime.timedelta(microseconds=sub_time))[:-4] +'\n')
+	else:
+		srt_file.write(" --> " + str(datetime.timedelta(microseconds=sub_time)) +'.00\n')
+		
+
+	srt_file.write(i + '\n\n')
+	counter = counter + 1
+
+
+# convert srt to ass
+convert_sub = "ffmpeg -i " + videos + scene + ".srt " + videos + scene +".ass"
+process = subprocess.Popen(convert_sub ,shell=True)
+process.wait()
+
+
+# position the timestamp in the video and change font size
+with open(videos + scene + ".ass") as ass_file:
+	lines = ass_file.readlines()
+	new_lines = []
+	for line in lines:
+		if line.startswith('Dialogue'):
+			line = line[:43] + line[43:44].replace('0', '300') + line[44:] 
+			line = line[:49] + line[49:50].replace('0', '260') + line[50:]
+			new_lines.extend(line)
+		elif line.startswith('Style'):
+			line = line[:21] + line[21:23].replace('16', '12') + line[23:]
+			new_lines.extend(line)
+		else:
+			new_lines.extend(line)
+	
+
+with open(videos + scene + ".ass", "w") as ass_file:
+	ass_file.writelines(new_lines)
+
+
+
+# add subtitles to stabilized video
+add_sub = "ffmpeg -i "+ videos + scene + "-time_lapse_stabilized.mp4 -vf ass=" + videos + scene +".ass " + videos + scene + "-time_lapse_stabilized_subs.mp4"
+process = subprocess.Popen(add_sub ,shell=True)
+process.wait()
+
+
+os.remove('transforms.trf')
+os.remove(videos + scene + '.srt')
