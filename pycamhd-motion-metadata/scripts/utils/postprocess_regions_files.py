c5445c46fd50b43fc6ca413199bfb31a33781a07
Bhuvan <msbhuvan@uw.edu>
2018-11-18 18:31:27 -0800

    Modify and organize code.

--- pycamhd-motion-metadata/scripts/utils/postprocess_regions_files.py
+++ pycamhd-motion-metadata/scripts/utils/postprocess_regions_files.py
@@ -8,7 +8,8 @@ the images dir created by the make_regions_proof_sheet.py script.
 # TODO: Extend the script to work independently to download the images.
 
 Usage: (Running from the root directory of the repository.)
-python scripts/utils/postprocess_regions_files.py ../RS03ASHS/PN03B/06-CAMHDA301/2018/08/0[456789] --img_path ../classification/proofsheets/20180804_20180809/images/
+python scripts/utils/postprocess_regions_files.py ../RS03ASHS/PN03B/06-CAMHDA301/2018/08/0[456789]
+    --image-path ../classification/proofsheets/20180804_20180809/images/
 
 """
 
@@ -25,12 +26,15 @@ import logging
 import os
 
 
-# TODO: Find the thresholds using some image processing method.
-# Threshold represents proportion of positive pixels after otsu_threshold and erosion on grayscale image
+# Threshold represents proportion of positive pixels after "yen" threshold (skimage) and erosion on grayscale image
 # for the scene (lower bound).
-scene_tag_thresholds = {
+FOREGROUND_PROPORTION_THRESHOLDS = {
     "d5A_p4_z1": 0.0135
 }
+
+# TODO: Hard threshold: If more than 8 mid_scenes, then scene correction is skipped.
+MAX_MID_SCENES = 8
+
 
 def get_args():
     parser = argparse.ArgumentParser(description="Postprocess the regions files to modify the scene_tag based "
@@ -43,10 +47,20 @@ def get_args():
                         default="d5A_p4",
                         help="The scene for which the respective scene_tags (various zooms) need to be corrected."
                              " Defaults to scene: d5A_p4.")
-    parser.add_argument('--img_path',
+    parser.add_argument('--image-path',
+                        dest='img_path',
                         required=True,
-                        help="The path containing the images. "
-                             "Provide path to the images folder in the output folder of make_regions_proof_sheet.py.")
+                        help="The path to the folder containing the images. "
+                             "Provide path to the 'images' folder in the output folder of make_regions_proof_sheet.py.")
+    parser.add_argument('--image-size',
+                        dest='img_size',
+                        nargs='?',
+                        default='320x240',
+                        help="The size of the thumbnail image to refer. Default: 320x240.")
+    parser.add_argument('--image-ext',
+                        dest='img_ext',
+                        default='jpg',
+                        help="The image file extension. Default: jpg.")
     parser.add_argument('--overwrite',
                         action="store_true",
                         help="Overwrite the existing regions_file to include the modifications based on "
@@ -57,24 +71,33 @@ def get_args():
     parser.add_argument("--log",
                         default="INFO",
                         help="Specify the log level. Default: INFO.")
-    return parser.parse_args()
+
+    args = parser.parse_args()
+
+    # Set up the Lazycache connection.
+    args.qt = camhd.lazycache(args.lazycache)
+
+    # Parse the img_size argument.
+    toks = args.img_size.split("x")
+    args.img_size = (int(toks[0]), int(toks[1]))
+
+    return args
 
 
-def _correct_sequencing_region_zoom_d5A_p4(regions_file, img_path, qt):
+def _correct_sequencing_region_zoom_d5A_p4(regions_file, img_path, img_ext="jpg"):
     """
     Note that this catches only the first occurrence of the initial and the final scene_tags.
 
     """
-    # TODO: Current supported scene is d5A_p4.
     scene = "d5A_p4"
     inferred_by = "p4SequenceCheck"
 
-    def _get_foreground_proportion(img_path):
+    def _get_foreground_proportion(sample_frame_img_path):
         """
         Applies OTSU threshold on the image followed by an erosion, then takes the proportion of positive pixels.
 
         """
-        img = imread(img_path, as_gray=True)
+        img = imread(sample_frame_img_path, as_gray=True)
         thresh = threshold_yen(img)
         img_binary = img > thresh
         eroded_img = erosion(img_binary, square(3))
@@ -97,9 +120,9 @@ def _correct_sequencing_region_zoom_d5A_p4(regions_file, img_path, qt):
             end_scene_index = i
 
     if initial_scene_index is None or end_scene_index is None:
-        logging.info("The initial or the end scene_tag is not found. Skipping the file: %s" % (regions_file.basename))
+        logging.warning("The initial or the end scene_tag is not found. Skipping the file: %s"
+                        % (regions_file.basename))
         return
-
 
     # TODO: Assuming only two mid scenes exist which are z1 and z2.
     z1_scene_tag = "%s_%s" % (scene, "z1")
@@ -109,36 +132,22 @@ def _correct_sequencing_region_zoom_d5A_p4(regions_file, img_path, qt):
     z2_found = False
     mid_scene_indices = range(initial_scene_index + 1, end_scene_index)
 
-    # TODO: Hard threshold: If more than 8 mid_scenes, then return.
-    if len(mid_scene_indices) >= 8:
+    if len(mid_scene_indices) >= MAX_MID_SCENES:
+        logging.warning("Skipping the regions file as there are more than %s mid scenes: %s" % (MAX_MID_SCENES, url))
         return
 
     for i in mid_scene_indices:
         region = regions_file.static_at(i)
         sample_frame = region.start_frame + 0.5 * (region.end_frame - region.start_frame)
-
-        # TODO: The imageext hardcoded to jpg as it is the default in the make_regions_proof_sheet.py script.
         sample_frame_path_thumbnail = os.path.join(img_path, "%s_%d_thumbnail.%s"
-                                                   % (os.path.splitext(os.path.basename(url))[0], sample_frame, "jpg"))
-        if not os.path.exists(sample_frame_path_thumbnail):
-            # TODO: img_size taken from the default of make_regions_proof_sheet.py script.
-            img_size = (320, 240)
-            sample_frame_path = os.path.join(img_path, "%s_%d.%s"
-                                             % (os.path.splitext(os.path.basename(url))[0], sample_frame, "jpg"))
-            logging.info("Fetching frame %d from %s for contact sheet" % (sample_frame, os.path.basename(url)))
-            img = qt.get_frame(url, sample_frame, format="jpg")
-            img.save(sample_frame_path)
-            img.thumbnail(img_size)  # PIL.thumbnail preserves aspect ratio
-            img.save(sample_frame_path_thumbnail)
-
-        if z1_scene_tag not in scene_tag_thresholds:
-            raise ValueError("The scene_tag %s is not supported by the scene_tag_thresholds threshold check."
-                             % z1_scene_tag)
+                                                   % (os.path.splitext(os.path.basename(url))[0],
+                                                      sample_frame,
+                                                      img_ext))
 
         foreground_proportion = _get_foreground_proportion(sample_frame_path_thumbnail)
         logging.info("Foreground proportion of region (%s, %s) is: %s (thumbnail path: %s)"
                      % (region.start_frame, region.end_frame, foreground_proportion, sample_frame_path_thumbnail))
-        is_z1 = foreground_proportion > scene_tag_thresholds[z1_scene_tag]
+        is_z1 = foreground_proportion > FOREGROUND_PROPORTION_THRESHOLDS[z1_scene_tag]
 
         if is_z1:
             if z2_found:
@@ -163,25 +172,53 @@ def _correct_sequencing_region_zoom_d5A_p4(regions_file, img_path, qt):
                                                                           z2_scene_tag))
             z2_found = True
 
-    # Logging.
+    # Logging warnings.
     if not z1_found and not z2_found:
         logging.warning("p4_z1 and p4_z2 were not found.")
-
     if not z1_found:
         logging.warning("p4_z1 was not found.")
     if not z2_found:
         logging.warning("p4_z2 was not found.")
 
 
-def postprocess_using_sequencing_prior(args):
-    # TODO: Currently, supports only scene d5A_p4.
+
+def _get_all_sample_frames(regions_file, img_path, qt, img_size=(320, 240), img_ext="jpg"):
+    """
+    Checks whether a sample frame exists for each static region. If not, it downloads and creates a thumbnail.
+
+    """
+    for region in regions_file.static_regions():
+        url = regions_file.mov
+
+        sample_frame = region.start_frame + 0.5 * (region.end_frame - region.start_frame)
+        sample_frame_path_thumbnail = os.path.join(img_path, "%s_%d_thumbnail.%s"
+                                                   % (os.path.splitext(os.path.basename(url))[0],
+                                                      sample_frame,
+                                                      img_ext))
+
+        if not os.path.exists(sample_frame_path_thumbnail):
+            sample_frame_path = os.path.join(img_path, "%s_%d.%s"
+                                             % (os.path.splitext(os.path.basename(url))[0], sample_frame, img_ext))
+            logging.info("Fetching frame %d from %s for contact sheet" % (sample_frame, os.path.basename(url)))
+            img = qt.get_frame(url, sample_frame, format=img_ext)
+            img.save(sample_frame_path)
+            img.thumbnail(img_size)  # PIL.thumbnail preserves aspect ratio
+            img.save(sample_frame_path_thumbnail)
+
+
+def postprocess(args):
     if args.scene != "d5A_p4":
-        raise NotImplementedError("Supported scene are: p4.")
+        raise NotImplementedError("Supported scene are: d5A_p4.")
 
     def _process(infile):
         logging.info("Postprocessing the regions file: {}".format(infile))
         regions_file = mmd.RegionFile.load(infile)
-        _correct_sequencing_region_zoom_d5A_p4(regions_file, args.img_path, args.qt)
+        logging.info("Getting all sample frames.")
+        _get_all_sample_frames(regions_file, args.img_path, args.qt, args.img_size, args.img_ext)
+
+        # TODO: Currently, supports only scene d5A_p4.
+        logging.info("Correcting scene_tags for d5A_p4 based on prior ordering of z1 and z2.")
+        _correct_sequencing_region_zoom_d5A_p4(regions_file, args.img_path, args.img_ext)
 
         if args.overwrite:
             logging.warning("Overwriting the regions_file: %s" % regions_file.mov)
@@ -197,9 +234,7 @@ def postprocess_using_sequencing_prior(args):
                 _process(infile)
 
 
-
 if __name__ == "__main__":
     args = get_args()
     logging.basicConfig(level=args.log.upper())
-    args.qt = camhd.lazycache(args.lazycache)
-    postprocess_using_sequencing_prior(args)
+    postprocess(args)
